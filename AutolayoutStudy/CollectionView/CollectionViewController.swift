3b3c80fd45c512af9cd05409ab300fa46df474fb
JaekyungYou <youjk96@gmail.com>
2020-12-16 12:48:10 +0900

    bug: Issue#1 UIViewAlertForUnsatisfiableConstraints

--- AutolayoutStudy/CollectionView/CollectionViewController.swift
+++ AutolayoutStudy/CollectionView/CollectionViewController.swift
@@ -10,8 +10,38 @@ import UIKit
 
 class CollectionViewController: UIViewController {
     
+    @IBOutlet weak var myCollectionView: UICollectionView!
+    
+    @IBOutlet weak var mySegment: UISegmentedControl!
+    
+    fileprivate let systemImageNameArray = [
+           "moon", "zzz", "sparkles", "cloud", "tornado", "smoke.fill", "tv.fill", "gamecontroller", "headphones", "flame", "bolt.fill", "hare", "tortoise", "moon", "zzz", "sparkles", "cloud", "tornado", "smoke.fill", "tv.fill", "gamecontroller", "headphones", "flame", "bolt.fill", "hare", "tortoise", "ant", "hare", "car", "airplane", "heart", "bandage", "waveform.path.ecg", "staroflife", "bed.double.fill", "signature", "bag", "cart", "creditcard", "clock", "alarm", "stopwatch.fill", "timer"
+       ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        myCollectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        myCollectionView.delegate = self
+        myCollectionView.dataSource = self
     }
     
+}
+
+// UICollectionViewDelegate : 액션 관련, UICollectionViewDataSource : 데이터 관련
+extension CollectionViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return systemImageNameArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MyCollectionCell", for: indexPath) as! MyCollectionCell
+
+        // CollectionViewCell에서 작업해도 됨 
+//        cell.systemImage.image = UIImage(systemName: self.systemImageNameArray[indexPath.item]) // 이미지 설정
+//        cell.systemLabel.text = self.systemImageNameArray[indexPath.item] // 라벨 설정
+        cell.imageName = systemImageNameArray[indexPath.item]
+        return cell
+        
+    }
 }
---
+++ AutolayoutStudy/CollectionView/MyCollectionCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyCollectionCell.swift
+//  AutolayoutStudy
+//
+//  Created by jaekyung you on 2020/12/16.
+//
+
+import Foundation
+import UIKit
+
+
+class MyCollectionCell: UICollectionViewCell {
+    
+    @IBOutlet weak var systemImage: UIImageView!
+    
+    @IBOutlet weak var systemLabel: UILabel!
+    
+    var imageName: String = "" {
+        didSet {
+            self.systemImage.image = UIImage(systemName: imageName) // 이미지 설정
+            self.systemLabel.text = self.imageName // 라벨 설정
+        }
+    }
+}
